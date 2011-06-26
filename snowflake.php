80216ce418a4bd5b9d542362e78e893ce1f02cfa
Will Norris <will@willnorris.com>
2011-06-26 13:15:01 -0700

    initial commit

---
+++ snowflake.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ Plugin Name: Snowflake
+ Plugin URI: http://github.com/willnorris/wordpress-snowflake
+ Description: Content syndicator for WordPress
+ Author: Will Norris
+ Author URI: http://willnorris.com/
+ Version: 1.0
+ License: Dual GPL (http://www.fsf.org/licensing/licenses/info/GPLv2.html) and Modified BSD (http://www.fsf.org/licensing/licenses/index_html#ModifiedBSD)
+ Text Domain: hum
+ */
